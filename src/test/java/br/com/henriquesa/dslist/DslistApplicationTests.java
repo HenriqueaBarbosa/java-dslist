7f405ea0f32a8c06f4974ea8c1e6048f9018b38e
Henrique Barbosa <henrique.abdiel@gmail.com>
2023-05-09 11:14:03 -0300

    docs: criação das pastas do projeto

---
+++ src/test/java/br/com/henriquesa/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.henriquesa.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
